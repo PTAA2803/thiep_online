f5f142e6d686d255c24e7e724dc09f9735a82f2e
PTAA2803 <123221945+PTAA2803@users.noreply.github.com>
2026-01-05 16:07:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const intro = document.getElementById("intro");
     const bookWrapper = document.getElementById("book-wrapper");
     const book = document.querySelector(".book");
-    const btn = document.getElementById("open-btn"); // Lấy nút bấm
+    const btn = document.getElementById("open-btn");
   
-    // 1. Chạy Intro xong thì hiện sách (Giữ nguyên)
+    // 1. Chạy Intro xong thì hiện sách (đóng)
     setTimeout(() => {
       intro.style.opacity = "0";
       
@@ -14,20 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
         
         setTimeout(() => {
           bookWrapper.classList.add("visible");
-          // LƯU Ý: Ở đây không còn lệnh tự động mở book.classList.add("open") nữa
         }, 50);
         
       }, 800); 
   
     }, 2500);
   
-    // 2. Xử lý sự kiện bấm nút
+    // 2. Xử lý sự kiện bấm nút icon
     btn.addEventListener("click", () => {
       // Thêm class mở sách
       book.classList.add("open");
       
-      // Ẩn nút đi sau khi bấm để đỡ vướng
+      // Ẩn nút icon đi
       btn.style.opacity = "0";
-      btn.style.pointerEvents = "none"; // Không cho bấm nữa
+      btn.style.pointerEvents = "none";
     });
   });
