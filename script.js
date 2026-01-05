0d1bd9fce9239a4e1f068312562db862076a0f68
PTAA2803 <123221945+PTAA2803@users.noreply.github.com>
2026-01-05 15:14:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-setTimeout(() => {
-    document.getElementById("intro").style.display = "none";
-    const card = document.getElementById("card");
-    card.classList.remove("hidden");
-  }, 2500); // 2.5 giây sau mới mở thiệp
+document.addEventListener("DOMContentLoaded", () => {
+    const intro = document.getElementById("intro");
+    const bookWrapper = document.getElementById("book-wrapper");
+    const book = document.querySelector(".book");
   
+    // Thời gian Intro: 2.5s
+    setTimeout(() => {
+      intro.style.opacity = "0";
+      
+      setTimeout(() => {
+        intro.style.display = "none";
+        bookWrapper.classList.remove("hidden");
+        
+        setTimeout(() => {
+          bookWrapper.classList.add("visible");
+          
+          // Mở sách
+          setTimeout(() => {
+            book.classList.add("open");
+          }, 800);
+          
+        }, 50);
+      }, 800); 
+  
+    }, 2500);
+  });
